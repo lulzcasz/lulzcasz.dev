4b6be4d665412aa081721457af43dc4edf8aa611
Lucas Reis <reisdossousalucasedson@gmail.com>
2025-11-11 15:04:53 -0300

    feat(blog): add posts by category and tag views in post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import index, post_list, post_detail
+from blog.views import index, post_list, post_detail, posts_by_category, posts_by_tag
 
 urlpatterns = [
     path('', index, name="index"),
-    path('todos-os-posts/', post_list, name="post-list"),
-    path('<slug:post_slug>', post_detail, name="post-detail"),
+    path('posts/', post_list, name="post-list"),
+    path('posts/<slug:post_slug>', post_detail, name="post-detail"),
+    path('categorias/<slug:category_slug>', posts_by_category, name="posts-by-category"),
+    path('tags/<path:tag_full_path>', posts_by_tag, name="posts-by-tag"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from posts.models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from posts.models import Post, Tag
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -25,9 +25,58 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'page_obj': page_obj})
 
 
+from logging import getLogger
+logger = getLogger(__name__)
+
+
 def post_detail(request, post_slug):
-    return render(
-        request,
-        'blog/post_detail.html',
-        {"post": get_object_or_404(Post, slug=post_slug, status=Post.Status.PUBLISHED)},
-    )
+    post = get_object_or_404(Post, slug=post_slug, status=Post.Status.PUBLISHED)
+
+    tags = [] 
+    created_nodes = {} 
+
+    for tag in post.tags.all(): 
+        tag_hierarchy = list(tag.get_ancestors()) + [tag] 
+
+        current_parent_list = tags
+
+        for t in tag_hierarchy:
+            unique_key = t.full_path 
+
+            if unique_key in created_nodes:
+                node = created_nodes[unique_key]
+                
+            else:
+                node = {
+                    'name': t.name,
+                    'full_path': t.full_path,
+                    'children': []
+                }
+
+                created_nodes[unique_key] = node
+                
+                current_parent_list.append(node)
+
+            current_parent_list = node['children']
+
+    ctx = {
+        'post': post,
+        'tags': tags,
+    }
+
+    return render(request, 'blog/post_detail.html', ctx)
+
+
+def posts_by_category(request, category_slug):
+    return render(request, 'blog/posts_by_category.html', {'posts': 'Post.objects.all(category_slug = category_slug)'})
+
+
+def posts_by_tag(request, tag_full_path):
+    from logging import getLogger
+    logger = getLogger(__name__)
+
+    logger.warning(tag_full_path)
+
+    posts = Tag.objects.get(full_path=tag_full_path).posts
+
+    return render(request, 'blog/posts_by_tag.html', {'posts': posts})
