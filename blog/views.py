9a05b4e6162d1492e35049ea73f7604fe8433371
Lucas Reis <reisdossousalucasedson@gmail.com>
2025-11-13 11:55:32 -0300

    feat(blog/views): remove category hierarchy generation from post_detail

--- blog/views.py
+++ blog/views.py
@@ -30,41 +30,11 @@ logger = getLogger(__name__)
 
 
 def post_detail(request, post_slug):
-    post = get_object_or_404(Post, slug=post_slug, status=Post.Status.PUBLISHED)
-
-    categories = [] 
-    created_nodes = {} 
-
-    for category in post.categories.all(): 
-        category_hierarchy = list(category.get_ancestors()) + [category] 
-
-        current_parent_list = categories
-
-        for c in category_hierarchy:
-            unique_key = c.full_path 
-
-            if unique_key in created_nodes:
-                node = created_nodes[unique_key]
-                
-            else:
-                node = {
-                    'name': c.name,
-                    'full_path': c.full_path,
-                    'children': []
-                }
-
-                created_nodes[unique_key] = node
-                
-                current_parent_list.append(node)
-
-            current_parent_list = node['children']
-
-    ctx = {
-        'post': post,
-        'categories': categories,
-    }
-
-    return render(request, 'blog/post_detail.html', ctx)
+    return render(
+        request,
+        'blog/post_detail.html',
+        {'post': get_object_or_404(Post, slug=post_slug, status=Post.Status.PUBLISHED)}
+    )
 
 
 def posts_by_section(request, section_slug):
