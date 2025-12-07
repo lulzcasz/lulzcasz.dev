5baae2fa585ba9f70b7199123de8e78e0d759949
Lucas Reis <reisdossousalucasedson@gmail.com>
2025-12-07 02:48:22 -0300

    feat(posts/models)!: add Category description and Tutorial difficulty

--- posts/models.py
+++ posts/models.py
@@ -1,40 +1,41 @@
-from django.db.models import (
-    TextChoices,
-    CharField,
-    URLField,
-    SlugField,
-    DateTimeField,
-    UUIDField,
-    ManyToManyField,
-    BooleanField,
-    ImageField,
-    ForeignKey,
-    SET_NULL,
-)
-from django.utils.text import slugify
-from tinymce.models import HTMLField
 from uuid import uuid4
-from django.urls import reverse
-from treebeard.mp_tree import MP_Node
-from django.db import transaction
-from posts.utils.upload_to import post_image_path
-from polymorphic.models import PolymorphicModel
 
+from django.db import transaction
+from django.db.models import (
+    SET_NULL,
+    BooleanField,
+    CharField,
+    DateTimeField,
+    ForeignKey,
+    ImageField,
+    ManyToManyField,
+    SlugField,
+    TextChoices,
+    URLField,
+    UUIDField,
+)
+from django.urls import reverse
 from django.utils import timezone
+from django.utils.text import slugify
+from polymorphic.models import PolymorphicModel
+from posts.utils.upload_to import post_image_path
+from tinymce.models import HTMLField
+from treebeard.mp_tree import MP_Node
 
 
 class Category(MP_Node):
-    name = CharField('nome', max_length=32)
-    slug = SlugField(max_length=32, blank=True)
-    created_at = DateTimeField('criada em', auto_now_add=True)
-    updated_at = DateTimeField('atualizada em', auto_now=True)
-    full_path = CharField('caminho completo', max_length=128, unique=True, blank=True)
+    name = CharField("nome", max_length=60)
+    slug = SlugField(max_length=60, blank=True)
+    description = CharField("descrição", max_length=160, blank=True)
+    created_at = DateTimeField("criada em", auto_now_add=True)
+    updated_at = DateTimeField("atualizada em", auto_now=True)
+    full_path = CharField("caminho completo", max_length=128, unique=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
-            
-        ancestor_slugs = list(self.get_ancestors().values_list('slug', flat=True))
+
+        ancestor_slugs = list(self.get_ancestors().values_list("slug", flat=True))
         ancestor_slugs.append(self.slug)
 
         new_full_path = "/".join(ancestor_slugs)
@@ -43,48 +44,48 @@ class Category(MP_Node):
 
         with transaction.atomic():
             super().save(*args, **kwargs)
-            descendants = self.get_descendants()   
+            descendants = self.get_descendants()
             for descendant in descendants:
                 descendant.save()
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
-        verbose_name = 'categoria'
+        verbose_name = "categoria"
 
     def get_absolute_url(self):
         return reverse(
-            'posts-by-category', kwargs={'category_full_path': self.full_path}
+            "posts-by-category", kwargs={"category_full_path": self.full_path}
         )
 
 
 class Post(PolymorphicModel):
     class Status(TextChoices):
-        DRAFT = 'draft', 'Rascunho'
-        PUBLISHED = 'published', 'Publicado'
-        
+        DRAFT = "draft", "Rascunho"
+        PUBLISHED = "published", "Publicado"
+
     uuid = UUIDField(default=uuid4, editable=False, unique=True, db_index=True)
     author = ForeignKey(
-        'auth.User',
+        "auth.User",
         SET_NULL,
-        verbose_name='autor',
+        verbose_name="autor",
         null=True,
         blank=True,
-        related_name='posts',
+        related_name="posts",
     )
-    title = CharField('título', max_length=60, unique=True)
+    title = CharField("título", max_length=60, unique=True)
     slug = SlugField(max_length=60, unique=True, blank=True)
-    description = CharField('descrição', max_length=160, blank=True)
-    cover = ImageField('capa', upload_to=post_image_path, blank=True)
-    video = URLField('vídeo', blank=True)
-    content = HTMLField('conteúdo', blank=True)
-    created_at = DateTimeField('criado em', auto_now_add=True)
-    updated_at = DateTimeField('atualizado em', auto_now=True)
-    published_at = DateTimeField('publicado em', null=True, editable=False)
+    description = CharField("descrição", max_length=160, blank=True)
+    cover = ImageField("capa", upload_to=post_image_path, blank=True)
+    video = URLField("vídeo", blank=True)
+    content = HTMLField("conteúdo", blank=True)
+    created_at = DateTimeField("criado em", auto_now_add=True)
+    updated_at = DateTimeField("atualizado em", auto_now=True)
+    published_at = DateTimeField("publicado em", null=True, editable=False)
     status = CharField(max_length=10, choices=Status.choices, default=Status.DRAFT)
     categories = ManyToManyField(
-        Category, verbose_name='categorias', related_name='posts', blank=True
+        Category, verbose_name="categorias", related_name="posts", blank=True
     )
 
     def save(self, *args, **kwargs):
@@ -104,28 +105,34 @@ class Post(PolymorphicModel):
                 self._cover_changed = True
 
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.title
-    
+
     @property
     def verbose_name(self):
         return self._meta.verbose_name
-    
+
     @property
     def verbose_name_plural(self):
         return self._meta.verbose_name_plural
-    
+
     def get_absolute_url(self):
         return reverse(
-            'post-detail',
+            "post-detail",
             kwargs={
-                'post_type': slugify(self.verbose_name_plural), 'post_slug': self.slug
-            }
+                "post_type": slugify(self.verbose_name_plural),
+                "post_slug": self.slug,
+            },
         )
 
 
 class Tutorial(Post):
+    class Difficulty(TextChoices):
+        BEGINNER = "beginner", "Iniciante"
+        INTERMEDIATE = "intermediate", "Intermediário"
+        ADVANCED = "advanced", "Avançado"
+
     prerequisites = ManyToManyField(
         'self',
         verbose_name='pré-requisitos',
@@ -133,21 +140,22 @@ class Tutorial(Post):
         symmetrical=False,
         related_name='tutorials',
     )
+    difficulty = CharField("dificuldade", max_length=15, choices=Difficulty.choices)
 
     class Meta:
-        verbose_name_plural = 'tutoriais'
+        verbose_name_plural = "tutoriais"
 
 
 class Article(Post):
-    is_review = BooleanField('é review', default=False)
-    is_opinion = BooleanField('é opinião', default=False)
+    is_review = BooleanField("é review", default=False)
+    is_opinion = BooleanField("é opinião", default=False)
 
     class Meta:
-        verbose_name = 'artigo'
+        verbose_name = "artigo"
 
 
 class News(Post):
-    is_breaking = BooleanField('é urgente', default=False)
+    is_breaking = BooleanField("é urgente", default=False)
 
     class Meta:
-        verbose_name = 'notícia'
+        verbose_name = "notícia"
