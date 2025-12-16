d171bcbee053f667ad0933c82de6491e3545875a
Lucas Reis <reisdossousalucasedson@gmail.com>
2025-12-16 14:29:10 -0300

    build: enable production static file serving via WhiteNoise

--- laboratoriodolyc/settings/base.py
+++ laboratoriodolyc/settings/base.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'laboratoriodolyc.urls'
@@ -153,8 +154,11 @@ STORAGES = {
             "location": "uploads",
         },
     },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    "staticfiles":     {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "OPTIONS": {
+            "location": "staticfiles",
+        },
     },
 }
 
--- laboratoriodolyc/settings/production.py
+++ laboratoriodolyc/settings/production.py
@@ -4,13 +4,4 @@ DEBUG = False
 
 AWS_S3_REGION_NAME = getenv("AWS_S3_REGION_NAME")
 
-STORAGES["staticfiles"].update(
-    {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "location": "staticfiles",
-        },
-    }
-)
-
 CSRF_TRUSTED_ORIGINS = getenv('DJANGO_CSRF_TRUSTED_ORIGINS').split(',')
